ea202edacf08bc5bcbc6bddf4ea08037a13fdbd8
Richard Chacon <rfchacon52@gmail.com>
2025-05-09 21:38:28 -0700

    Update app.js

--- app.js
+++ app.js
@@ -17,12 +17,16 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
+    .then(doc => {
     if (err) {
         console.log("error!! " + err)
     } else {
       //  console.log("MongoDB Connection Successful")
     }
 })
+.catch(err => {
+    console.error("Error finding document:", err);
+  });
 
 var Schema = mongoose.Schema;
 
